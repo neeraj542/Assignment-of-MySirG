d99705d9f3cc0212f7ac23f55ef55b01e96b32ba
Neeraj Meena IND <114648043+neeraj542@users.noreply.github.com>
2022-10-09 16:47:09 +0530

    Added Que-1 with solution in Assignment-2

---
+++ Que-1.c
@@ -0,0 +1,18 @@
+/*
+1. Write a program to print unit digit of a given number.
+*/
+
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int num1, num2;
+    printf("Enter any number: ");
+    scanf("%d",&num1);
+    
+    num2 = num1%10;
+    
+    printf("Unit digit of %d is %d",num1,num2);
+
+    return 0;
+}
